1c38bf1a028e929f109124e9575fa99d1c61f60b
laurie-moulin <laurie.moulin@laplateforme.io>
2021-01-06 19:54:16 +0100

    commit page d'accueil design

--- header.php
+++ header.php
@@ -1,13 +1,11 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">MEMORY</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+
+<nav class="navbar navbar-expand-lg ">
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-item nav-link active" href="index.php">Accueil </a>
       <a class="nav-item nav-link" href="inscription.php">Inscription</a>
       <a class="nav-item nav-link" href="connexion.php">Connexion</a>
+      <a class="nav-item nav-link" href="connexion.php">Jouer</a>
     </div>
   </div>
 </nav>
--- index.php
+++ index.php
@@ -1,27 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
     <title>Accueil</title>
 </head>
+
 <body>
 
-<header>
-<?php include("header.php"); ?>
-</header>
+    <header>
+        <?php include("header.php"); ?>
 
-<main>
+        <section class="home">
+            <img src="img/monsterred.png" class="monster">
+            <h1>MONSTER MEMORY</h1>
+            <img src="img/yellow.png" class="monster">
+        </section>
 
+        <a href="#presentation"><img src="img/arrow.png" class="arrow"></a>
 
+    </header>
 
-</main>
+    <main>
+        <div class="container_home">
+            <section id="presentation">
 
-<footer>
-<?php include("footer.php"); ?>
-</footer>
-    
+                <article class="dialogue">
+                    <h2> <br>Bienvenue sur la planête des monstres gentils ! </h2>
+                    <p>J'espere que tu a fais bon voyage jusqu'a notre planète... Nous sommes ici, moi et mes copain monstres, pour t'aider à travailler ta mémoire tout en s'amusant ! <br> Amuse toi bien et reviens nous voir quand tu veux! </p>
+                </article>
+
+            </section>
+
+            <section>
+                <img src="img/green.png">
+            </section>
+
+        </div>
+
+    </main>
+
+    <!-- <footer>
+
+</footer> -->
+
 </body>
 
 </html>
