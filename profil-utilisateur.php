3b31904e7cc32c799eadebd8c619e80c70965d25
chay <aicha.ouattara@laplateforme.io>
2021-01-07 14:08:35 +0100

    update page utilisateur

--- profil-utilisateur.php
+++ profil-utilisateur.php
@@ -1,4 +1,40 @@
 <?php
 
-
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+    <title>profil utilisateur</title>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-lg ">
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="index.php">Accueil </a>
+                <a class="nav-item nav-link" href="inscription.php">Inscription</a>
+                <a class="nav-item nav-link" href="connexion.php">Connexion</a>
+                <a class="nav-item nav-link" href="connexion.php">Jouer</a>
+            </div>
+        </div>
+    </nav>
+</header>
+<main>
+    <article>
+        <h1>Mon super article</h1>
+        <p>Bla bla bla</p>
+    </article>
+    <aside>
+        <p>Plus d'infos</p>
+    </aside>
+</main>
+<footer>Copyright 2015</footer>
+</body>
+</html>
+
--- wall-of-fame.php
+++ wall-of-fame.php
@@ -8,12 +8,23 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Top 10</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+    <title>top 10</title>
 </head>
 <body>
 <header>
-    <nav>
-        <a href="/">Home</a>
+    <nav class="navbar navbar-expand-lg ">
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="profil-utilisateur.php">Accueil </a>
+                <a class="nav-item nav-link" href="inscription.php">Inscription</a>
+                <a class="nav-item nav-link" href="connexion.php">Connexion</a>
+                <a class="nav-item nav-link" href="connexion.php">Jouer</a>
+            </div>
+        </div>
     </nav>
 </header>
 <main>
