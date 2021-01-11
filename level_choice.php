2f3f05cdb24d358e068cd8a35114ac8a662d0bc5
Laurie <laurie.moulin@laplateforme.io>
2021-01-11 14:23:17 +0100

    push level_choice

--- level_choice.php
+++ level_choice.php
@@ -1,29 +1,54 @@
 <?php
 
-	session_start();
- ?>
+session_start();
+?>
 
- <!doctype html>
- <html lang="en">
- <head>
- 	<!-- Required meta tags -->
- 	<meta charset="utf-8">
- 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!doctype html>
+<html lang="en">
 
- 	<!-- <base href="/"> -->
- 	<!-- Bootstrap CSS -->
- 	<link rel="stylesheet" type="text/css" href="css/memory.css">
- 	<link rel="stylesheet" type="text/css" href="css/index.css">
- 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
- 	<title>Play !</title>
- </head>
- <body>
- 	<?php include "includes/header.php"; ?>
+	<!-- <base href="/"> -->
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" type="text/css" href="css/memory.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+	<title>Play !</title>
+</head>
+
+<body>
+
+<header>
+	<?php include "includes/header.php"; ?>
+</header>
+
+<main class="main_level">
 	<div class="container">
-		<h1>Choisissez le niveau</h1>
+		<h1 class="title_level">Choisissez le niveau : Nombre de pairs</h1><br>
 	</div>
+
+<div class="container">
+	<article class="level"> 
+
+	<a href="memory.php?level=3&mode=one">3</a> 
+	<a href="memory.php?level=4&mode=one">4</a>
+	<a href="memory.php?level=5&mode=one">5</a>
+	<a href="memory.php?level=6&mode=one">6</a>
+	<a href="memory.php?level=7&mode=one">7</a>
+	<a href="memory.php?level=8&mode=one">8</a>
+	<a href="memory.php?level=9&mode=one">9</a>
+	<a href="memory.php?level=10&mode=one">10</a>
+	<a href="memory.php?level=11&mode=one">11</a>
+	<a href="memory.php?level=12&mode=one">12</a>
+
+	</article>
+</div>
+
+</main>
 
 
 	<!-- Optional JavaScript -->
@@ -34,4 +59,5 @@
 	<?php include "includes/footer.php"; ?>
 
 </body>
+
 </html>
