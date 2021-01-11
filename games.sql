97057d8c79b848ad7ec5d93a0688bd7c0d35b524
chay <aicha.ouattara@laplateforme.io>
2021-01-11 09:55:11 +0100

    sql

---
+++ games.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le :  lun. 11 jan. 2021 à 08:54
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `memory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `games`
+--
+
+CREATE TABLE `games` (
+  `id` int(11) NOT NULL,
+  `time` time NOT NULL,
+  `grille` int(11) NOT NULL,
+  `datetime` datetime NOT NULL,
+  `score` int(11) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `games`
+--
+
+INSERT INTO `games` (`id`, `time`, `grille`, `datetime`, `score`, `id_utilisateur`) VALUES
+(1, '00:07:14', 6, '2021-01-05 12:42:31', 0, 1),
+(2, '00:06:39', 3, '2021-01-05 13:16:39', 0, 2),
+(3, '00:03:35', 12, '2021-01-08 09:00:35', 3, 3),
+(4, '00:20:39', 6, '2021-01-07 09:01:39', 6, 4),
+(5, '00:10:39', 3, '2021-01-07 09:01:39', 6, 5);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `games`
+--
+ALTER TABLE `games`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
