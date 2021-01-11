796780f021df2e3e012e5e75017592820e7e9016
aicha <aicha.ouattara@laplateforme.io>
2021-01-11 16:17:00 +0100

    update

--- Wof.php
+++ Wof.php
@@ -1,8 +1,5 @@
 <?php
-// define('MYSQL_SERVEUR', 'localhost');
-// define('MYSQL_UTILISATEUR', 'root');
-// define('MYSQL_MOTDEPASSE', 'root');
-// define('MYSQL_BASE', 'memory');
+require_once 'class/user.php';
 
 
 class Wof
@@ -16,37 +13,37 @@ class Wof
 
     public static function insert_wof($time, $grid, $datetime, $score, $id_utilisateur)
     {
-        $bdd = new PDO("mysql:host=" . MYSQL_SERVEUR . ";dbname=" . MYSQL_BASE . "", MYSQL_UTILISATEUR, MYSQL_MOTDEPASSE);
-        $req_insert = $bdd->prepare("INSERT INTO games ( time , grille , datetime, score, id_utilisateur) VALUES (?, ?, ?, ?, ?)") ;
+        $db = new PDO("mysql:host=" . MYSQL_SERVEUR . ";dbname=" . MYSQL_BASE . "", MYSQL_UTILISATEUR, MYSQL_MOTDEPASSE);
+        $req_insert = $db->prepare("INSERT INTO games ( time , grille , datetime, score, id_utilisateur) VALUES (?, ?, ?, ?, ?)") ;
         $req_insert->execute([$time, $grid, $datetime, $score, $id_utilisateur]);
     }
 
     //Recuparation de toutes les infos du joueur : progression individuelle
-    public function users_profil_details()
+    public static function users_profil_details()
     {
-        $bdd = new PDO("mysql:host=" . MYSQL_SERVEUR . ";dbname=" . MYSQL_BASE . "", MYSQL_UTILISATEUR, MYSQL_MOTDEPASSE);
-        $req = $bdd->prepare("SELECT  avatar, login, time, score, grille, datetime as date  FROM games INNER JOIN utilisateurs WHERE games.id_utilisateur = utilisateurs.id");
+        $db = new PDO("mysql:host=" . MYSQL_SERVEUR . ";dbname=" . MYSQL_BASE . "", MYSQL_UTILISATEUR, MYSQL_MOTDEPASSE);
+        $req = $db->prepare("SELECT login, time, score, grille, datetime as date  FROM games INNER JOIN utilisateurs WHERE games.id_utilisateur = utilisateurs.id");
         $req->execute();
-        $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
         $bdd = NULL;
         return $resultat;
     }
 
     //3 derniers meilleurs scores réalisés
-    public function users_progress_score($id)  //$grid ou $id ?
+    public static function users_progress_score($id)  //$grid ou $id ?
     {
-        $bdd = new PDO("mysql:host=" . MYSQL_SERVEUR . ";dbname=" . MYSQL_BASE . "", MYSQL_UTILISATEUR, MYSQL_MOTDEPASSE);
-        $req= $bdd->prepare("SELECT time as temps, score, grille as pairs , datetime as date  FROM games WHERE id_utilisateur = ? ORDER BY games.score DESC LIMIT 3");
+        $db = new PDO("mysql:host=" . MYSQL_SERVEUR . ";dbname=" . MYSQL_BASE . "", MYSQL_UTILISATEUR, MYSQL_MOTDEPASSE);
+        $req= $db->prepare("SELECT time as temps, score, grille as pairs , datetime as date  FROM games WHERE id_utilisateur = ? ORDER BY games.score DESC LIMIT 3");
         $req->execute([$id]);
-        $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
         $bdd = NULL;
         return $resultat;
     }
     //Les 3 dernieres parties qui ont été jouer par l'user par temps
-    public function users_progress($id)
+    public static function users_progress($id)
     {
-        $bdd = new PDO("mysql:host=" . MYSQL_SERVEUR . ";dbname=" . MYSQL_BASE . "", MYSQL_UTILISATEUR, MYSQL_MOTDEPASSE);
-        $req = $bdd->prepare("SELECT time as temps, score, grille as pairs , datetime as date FROM games INNER JOIN utilisateurs WHERE games.id_utilisateur = ? ORDER BY games.time desc LIMIT 3");
+        $db = new PDO("mysql:host=" . MYSQL_SERVEUR . ";dbname=" . MYSQL_BASE . "", MYSQL_UTILISATEUR, MYSQL_MOTDEPASSE);
+        $req = $db->prepare("SELECT time as temps, score, grille as pairs , datetime as date FROM games INNER JOIN utilisateurs WHERE games.id_utilisateur = ? ORDER BY games.time desc LIMIT 3");
         $req->execute([$id]);
         $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
         $bdd = NULL;
@@ -57,8 +54,8 @@ class Wof
     //dix meilleurs parties par grille:niveau classé par temps
     public function top_10_time($grid)
     {
-        $bdd = new PDO("mysql:host=" . MYSQL_SERVEUR . ";dbname=" . MYSQL_BASE . "", MYSQL_UTILISATEUR, MYSQL_MOTDEPASSE);
-        $req = $bdd->prepare("SELECT avatar, login, time as temps, score, grille, datetime as date  FROM games inner join utilisateurs on games.id_utilisateur =  utilisateurs.id
+        $db = new PDO("mysql:host=" . MYSQL_SERVEUR . ";dbname=" . MYSQL_BASE . "", MYSQL_UTILISATEUR, MYSQL_MOTDEPASSE);
+        $req = $db->prepare("SELECT avatar, login, time as temps, score, grille, datetime as date  FROM games inner join utilisateurs on games.id_utilisateur =  utilisateurs.id
         WHERE grille = ? ORDER BY games.time ASC LIMIT 10 ");
         $req->execute([$grid]);
         $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
@@ -69,8 +66,8 @@ class Wof
     //dix meilleurs parties par grille:niveau classé par score
     public function top_10_score($grid)
     {
-        $bdd = new PDO("mysql:host=" . MYSQL_SERVEUR . ";dbname=" . MYSQL_BASE . "", MYSQL_UTILISATEUR, MYSQL_MOTDEPASSE);
-        $req = $bdd->prepare("SELECT avatar, login, time as temps, score, grille, datetime as date FROM games inner join utilisateurs on games.id_utilisateur =  utilisateurs.id
+        $db = new PDO("mysql:host=" . MYSQL_SERVEUR . ";dbname=" . MYSQL_BASE . "", MYSQL_UTILISATEUR, MYSQL_MOTDEPASSE);
+        $req = $db->prepare("SELECT avatar, login, time as temps, score, grille, datetime as date FROM games inner join utilisateurs on games.id_utilisateur =  utilisateurs.id
         WHERE grille = ? ORDER BY games.score ASC LIMIT 10 ");
         $req->execute([$grid]);
         $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
@@ -81,8 +78,8 @@ class Wof
     //Les 3 dernieres parties qui ont été jouer par id de la table game decroissant
     public function three_last_games()
     {
-        $bdd = new PDO("mysql:host=" . MYSQL_SERVEUR . ";dbname=" . MYSQL_BASE . "", MYSQL_UTILISATEUR, MYSQL_MOTDEPASSE);
-        $req = $bdd->prepare("SELECT avatar, login, time as temps, score, grille, datetime as date FROM games INNER JOIN utilisateurs WHERE games.id_utilisateur = utilisateurs.id ORDER BY games.id desc LIMIT 3");
+        $db = new PDO("mysql:host=" . MYSQL_SERVEUR . ";dbname=" . MYSQL_BASE . "", MYSQL_UTILISATEUR, MYSQL_MOTDEPASSE);
+        $req = $db->prepare("SELECT avatar, login, time as temps, score, grille, datetime as date FROM games INNER JOIN utilisateurs WHERE games.id_utilisateur = utilisateurs.id ORDER BY games.id desc LIMIT 3");
         $req->execute();
         $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
         $bdd = NULL;
